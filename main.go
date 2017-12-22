6f964f688ac91344a8fd5d8bd3a7ddc47e22c785
zunda <zundan@gmail.com>
2017-12-22 10:00:30 -1000

    Add H12

--- main.go
+++ main.go
@@ -7,17 +7,26 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func writeUsage(w http.ResponseWriter) {
 	fmt.Fprintf(w, `
 <html><title>status-code-app</title><body><h1>status-code-app</h1>
-<p>Hi, I'll respond to requests with specified status code. Try something like</p>
+<p>Hi, I'll respond to requests with specified status code or trigger errors. Try something like</p>
 <ul>
-<li><a href="/418">/418</a> - I'm a teapot.
+<li><a href="/418">/418</a> - I'm a teapot
+<li><a href="/H12">/H12</a> - Request timeout
 </ul>
 </body></html>
 `)
+}
+
+func h12Server(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Waiting for 35 sec to trigger an H12 - Request timeout")
+	time.Sleep(35 * time.Second)
+	fmt.Fprintf(w, "Hi. did you see an H12 from Heroku router in the app logs?")
+	log.Printf("Done")
 }
 
 func statusCodeServer(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +54,7 @@ func main() {
 
 	h := http.NewServeMux()
 	h.HandleFunc("/favicon.ico", http.NotFound)
+	h.HandleFunc("/H12", h12Server)
 	h.HandleFunc("/", statusCodeServer)
 
 	log.Println("Listening at port " + port)
