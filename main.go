844d2c5b0044e4e4be982161efebca01ff39b370
zunda <zundan@gmail.com>
2020-03-04 16:07:15 -1000

    Add /H18 endpoint

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func writeUsage(w http.ResponseWriter) {
 <li><a href="/418">/418</a> - I'm a teapot
 <li><a href="/H12">/H12</a> - Request timeout
 <li><a href="/H13">/H13</a> - Connection closed without response
+<li><a href="/H18">/H18</a> - Server Request Interrupted
 </ul>
 <p>Source code is available at <a href="%s">%s</a></p>
 </body></html>
@@ -46,6 +47,22 @@ func h13Server(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Couldn't close the connection to trigegr an H13")
 	fmt.Fprintf(w, "Hi. I'm unavailable to trigger an H13.")
+}
+
+func h18Server(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Responding before reading request trigger an H18 - Server Request Interrupted")
+	hj, ok := w.(http.Hijacker)
+	if ok {
+		conn, _, err := hj.Hijack()
+		if err == nil {
+			fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-length: 13\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nHello, World\n")
+			conn.Close()
+			return
+		}
+	}
+
+	log.Printf("Couldn't hijack the connection to trigegr an H18")
+	fmt.Fprintf(w, "Hi. I'm unavailable to trigger an H18.")
 }
 
 func writeStandardResponse(w http.ResponseWriter, code int) {
@@ -78,6 +95,7 @@ func main() {
 	h.HandleFunc("/favicon.ico", http.NotFound)
 	h.HandleFunc("/H12", h12Server)
 	h.HandleFunc("/H13", h13Server)
+	h.HandleFunc("/H18", h18Server)
 	h.HandleFunc("/", statusCodeServer)
 
 	log.Println("Listening at port " + port)
